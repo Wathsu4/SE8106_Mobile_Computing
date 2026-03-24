ace0cc2ab9cdc8a7af26155fa76475c071d6ced0
D.G. Wathsala Rathnayake <dgwtrathnayake@std.appsc.sab.ac.lk>
2026-03-24 15:50:12 +0530

    24-03-2026 Practical create profile

--- app/src/main/java/com/example/practical2/MainActivity.kt
+++ app/src/main/java/com/example/practical2/MainActivity.kt
@@ -3,45 +3,63 @@ package com.example.practical2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.practical2.ui.theme.Practical2Theme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Practical2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            ProfileScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun ProfileScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ProfileTitle()
+        ProfileImage()
+        ProfileDescription()
+    }
+}
+
+@Composable
+fun ProfileTitle() {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Wathsala Rathnayake",
+        modifier = Modifier.padding(all = 16.dp)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Practical2Theme {
-        Greeting("Android")
-    }
+fun ProfileImage() {
+    Icon(
+        imageVector = Icons.Default.AccountCircle,
+        contentDescription = "Profile Image",
+        modifier = Modifier
+            .padding(all = 16.dp)
+            .size(size = 120.dp)
+    )
+}
+
+@Composable
+fun ProfileDescription() {
+    Text(
+        text = "Wathsala Rathnayake is a professional mobile app developer with over 20 years of experience",
+        modifier = Modifier.padding(all = 16.dp)
+    )
 }
